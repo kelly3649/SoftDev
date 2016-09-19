6bd03075b1ec6797bc11d9713e729a2b6354ccc0
Kelly Wang <kwang6@stuy.edu>
2016-09-19 12:54:12 -0400

    added template for python/html

---
+++ template4.py
@@ -0,0 +1,19 @@
+#!usr/bin/python
+print "Content-Type: text/html \n"
+print ""
+
+heading = '''
+<!DOCTYPE html>
+<html>
+<head>
+<title>InsertTitleHere</title>
+</head>
+<body>'''
+
+#Python functions!!!
+
+ending = '''
+</body>
+</html>
+
+print heading + ending
