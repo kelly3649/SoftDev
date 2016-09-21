fa00243fe46889b9c9f1079e9665181ba8c4bdba
Kelly Wang <kwang6@stuy.edu>
2016-09-20 20:49:02 -0400

    added app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "No hablo queso"
+
+def foo():
+    print "Kthis ia"
+if __name__=="__main__":
+    app.run()
+
+    
